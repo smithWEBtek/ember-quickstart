b84eaac7b938b188da1b6bc8e9809ed59e6e2c1e
Brad Smith <brad@smithwebtek.com>
2019-09-10 20:42:01 -0400

    Add fruits

--- app/router.js
+++ app/router.js
@@ -6,9 +6,10 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
   this.route('scientists');
   this.route('programmers');
+  this.route('fruits');
 });
 
 export default Router;
